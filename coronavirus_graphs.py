b7133c0dd50bee8b7dee0ba779bfd16eec6ec9f7
Deniz Aslan <d_b_aslan@windowslive.com>
2021-05-06 17:59:25 +0300

    Started Part III

---
+++ coronavirus_graphs.py
@@ -0,0 +1,46 @@
+import coronavirus_statistics as cs
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def regions_piechart(region, show=False):
+    """hello
+    """
+    region_array = cs.region_data()
+    index = np.where(region_array == region)[0][0]
+    n_deaths = int(region_array[index, 2])
+    n_recovered = int(region_array[index, 1]) - n_deaths
+
+    plt.figure(figsize=(6, 6))
+    labels = "dead", "recovered"
+    fractions = [n_deaths, n_recovered]
+    plt.pie(fractions, labels=labels, autopct="%1.1f%%")
+    plt.title(f"Fraction of COVID-19 patients in {region} who died")
+    plt.savefig("graphs/region_piechart.png")
+    if show:
+        plt.show()
+
+
+
+def countries_barchart(show=False):
+    """hello
+    """
+
+
+def highest_mortality(n, k, show=False):
+    """
+    """
+
+
+def map(k, show=False):
+    """
+    """
+
+
+def main():
+    """
+    """
+
+
+if __name__ == "__main__":
+    main()
